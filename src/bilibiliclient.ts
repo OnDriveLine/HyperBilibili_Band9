cfdd1347e54bfa6d359388295b2991465839f224
Searchstars <searchstars@163.com>
2024-07-19 22:18:42 +0800

    收藏可用

--- src/bilibiliclient.ts
+++ src/bilibiliclient.ts
@@ -14,6 +14,8 @@ class BilibiliClient {
     private dedeUserID: string | null = null;
     private sid: string | null = null;
     private accountInfo: any | null = null;
+    private buvid3: any | null = null;
+    private buvid4: any | null = null;
 
     // 获取请求头
     private getHeaders(): { [key: string]: string } {
@@ -30,7 +32,7 @@ class BilibiliClient {
 
     // 构建Cookie字符串
     private getCookieString(): string {
-        return `SESSDATA=${this.sessData}; bili_jct=${this.biliJct}; DedeUserID=${this.dedeUserID}; sid=${this.sid}`;
+        return `SESSDATA=${this.sessData}; bili_jct=${this.biliJct}; DedeUserID=${this.dedeUserID}; sid=${this.sid}; buvid3=${this.buvid3}; buvid4=${this.buvid4}`;
     }
 
     // 发送GET请求的函数
@@ -41,10 +43,29 @@ class BilibiliClient {
                 responseType: 'json',
                 header: this.getHeaders()
             });
-            //console.log(response.data);
             return response.data;
         } catch (error) {
-            console.error(`请求失败，错误码 = ${error.code}`);
+            console.error(`GET请求失败，错误码 = ${error.code}`);
+            throw error;
+        }
+    }
+
+    // 发送POST请求的函数
+    private async postRequest(url: string, data: string, content_type: string): Promise<any> {
+        var headers = this.getHeaders();
+        headers["Content-Type"] = content_type
+
+        try {
+            const response = await fetch.fetch({
+                url: url,
+                responseType: 'json',
+                method: 'POST',
+                data: data,
+                header: headers
+            });
+            return response.data;
+        } catch (error) {
+            console.error(`POST请求失败，错误码 = ${error.code}`);
             throw error;
         }
     }
@@ -70,22 +91,33 @@ class BilibiliClient {
     async updateAccountInfo(): Promise<boolean> {
         const accoutInfo = await this.getRequest("https://api.bilibili.com/x/web-interface/nav");
         this.accountInfo = accoutInfo.data.data;
-        if(this.accountInfo){
+        if (this.accountInfo) {
             return true;
         }
         return false;
+    }
+
+    // 刷新BUVID
+    async updateBUVID() {
+        const url = "https://api.bilibili.com/x/frontend/finger/spi";
+        const response = await this.getRequest(url);
+
+        this.buvid3 = response.data.data.b_3
+        this.buvid4 = response.data.data.b_4
     }
 
     // 登录函数，使用本地存储的账号数据或通过二维码登录
     async login(send_req: boolean, interval: NodeJS.Timeout | null = null): Promise<{ success: boolean, message: string }> {
         const accountData = await this.getStoredAccountData();
         if (accountData) {
+            console.log("Loaded AccountData: ", accountData)
             this.sessData = accountData.sessData;
             this.biliJct = accountData.biliJct;
             this.dedeUserID = accountData.dedeUserID;
             this.sid = accountData.sid;
             console.log('使用存储的账号数据登录成功');
             await this.updateAccountInfo()
+            await this.updateBUVID()
             return {
                 success: true,
                 message: "登录成功"
@@ -120,6 +152,7 @@ class BilibiliClient {
                 await this.storeAccountData();
                 console.log('使用二维码登录并存储账号数据成功');
                 await this.updateAccountInfo()
+                await this.updateBUVID()
                 return {
                     success: true,
                     message: "登录成功"
@@ -150,9 +183,9 @@ class BilibiliClient {
     }
 
     // 获取目标用户创建的所有收藏夹的基本信息
-    async getUserFavouriteFolders(mid: string, type: number = 0, rid: string = null): Promise<any>{
+    async getUserFavouriteFolders(mid: string, type: number = 0, rid: string = null): Promise<any> {
         let url = `https://api.bilibili.com/x/v3/fav/folder/created/list-all?up_mid=${mid}&type=${type}`;
-        if(rid){
+        if (rid) {
             url += `&rid=${rid}`;
         }
         const response = await this.getRequest(url);
@@ -161,7 +194,7 @@ class BilibiliClient {
     }
 
     // 获取目标收藏夹元数据
-    async getFavouriteFolderMetadata(mlid: string): Promise<any>{
+    async getFavouriteFolderMetadata(mlid: string): Promise<any> {
         const url = `https://api.bilibili.com/x/v3/fav/folder/info?media_id=${mlid}`;
         const response = await this.getRequest(url);
 
@@ -169,14 +202,46 @@ class BilibiliClient {
     }
 
     // 获取目标收藏夹内容
-    async getFavouriteFolderContent(mlid: string, pn: number, ps: number = 10, keyword: string = null): Promise<any>{
+    async getFavouriteFolderContent(mlid: string, pn: number, ps: number = 10, keyword: string = null): Promise<any> {
         let url = `https://api.bilibili.com/x/v3/fav/resource/list?media_id=${mlid}&ps=${ps}&pn=${pn}`;
-        if(keyword){
+        if (keyword) {
             url += `&keyword=${keyword}`
         }
         const response = await this.getRequest(url);
 
         return response.data.data
+    }
+
+    async isVideoStaredByBVID(bvid: string): Promise<boolean> {
+        const url = `https://api.bilibili.com/x/v2/fav/video/favoured?aid=${bvid}`
+        const response = await this.getRequest(url);
+
+        return response.data.data.favoured
+    }
+
+    async starVideoToDefaultFavFolderByBVID(bvid: string): Promise<any> {
+        let defaultFolderID = 0
+        const folders = await this.getUserFavouriteFolders(this.accountInfo.mid)
+        folders.list.forEach(folder => {
+            if(folder.title == "默认收藏夹"){
+                defaultFolderID = folder.id
+            }
+        });
+        if(defaultFolderID == 0){
+            return false
+        }
+        this.getVideoInfoByBVID(bvid).then(async (videoInfo) => {
+            const aid = videoInfo.aid;
+            const url = `https://api.bilibili.com/x/v3/fav/resource/deal`
+            const data = `rid=${aid}&csrf=${this.biliJct}&type=2&add_media_ids=${defaultFolderID}&del_media_ids=`
+            const response = await this.postRequest(url, data, "application/x-www-form-urlencoded");
+
+            return response.data.code
+        }).catch(error => {
+            console.log("catched error: " + error)
+            return false
+        });
+        return false
     }
 
     // 退出登录（指在手表上删除存储的账号数据）
